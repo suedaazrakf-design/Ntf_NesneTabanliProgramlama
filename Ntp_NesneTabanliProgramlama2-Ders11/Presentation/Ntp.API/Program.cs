ed99c290f74b6203e9e49f930e49709bc36e6586
Sueda Azrak <suedaazrakf-design@github.com>
2026-05-11 14:10:27 +0300

    feat: Add ders11 content

---
+++ Ntp_NesneTabanliProgramlama2-Ders11/Presentation/Ntp.API/Program.cs
@@ -0,0 +1,48 @@
+using Ntp.Application;
+using Ntp.Mapper;
+using Ntp.Persistance;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var env = builder.Environment;
+
+builder.Configuration
+    .SetBasePath(env.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+
+builder.Services.AddPersistance(builder.Configuration);
+builder.Services.AddApplication();
+builder.Services.AddCustomMapper();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+
+app.MapScalarApiReference(options =>
+{
+    options.Title = "Nesne Programlama Projesi";
+    options.Theme = ScalarTheme.Default;
+
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
