31fdc65c273b604d9827eaca9f86bc652e5af533
Sueda Azrak <suedaazrakf-design@github.com>
2026-05-11 14:09:49 +0300

    feat: Add ders5 content

---
+++ Ntp_NesneTabanliProgramlama2-Ders5/Core/Ntp.Domain/Entities/Category.cs
@@ -0,0 +1,27 @@
+﻿using Ntp.Domain.Common;
+
+namespace Ntp.Domain.Entities;
+
+public class Category : EntityBase
+{
+    public Category()
+    {
+
+    }
+
+    public Category(string name, int subCategoryId, int sort)
+    {
+        Name = name;
+        SubCategoryId = subCategoryId;
+        Sort = sort;
+    }
+
+    public required string Name { get; set; }
+    public required int SubCategoryId { get; set; }
+    public required int Sort { get; set; }
+
+
+    public virtual ICollection<Detail> Details { get; set; }
+    public ICollection<Product> Products { get; set; }
+
+}
